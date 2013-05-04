f2b141943cace95aece6f4d30cfbfe9422ff03c7
ruel <ruel@ruel.me>
2013-05-04 21:36:49 +0800

    How did I miss this 12 days ago. Staged new files.

---
+++ FacebookLogin.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ADown
+{
+    partial class FacebookLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wbMain = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // wbMain
+            // 
+            this.wbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbMain.Location = new System.Drawing.Point(0, 0);
+            this.wbMain.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbMain.Name = "wbMain";
+            this.wbMain.Size = new System.Drawing.Size(499, 274);
+            this.wbMain.TabIndex = 0;
+            this.wbMain.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.wbMain_Navigated);
+            // 
+            // FacebookLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 274);
+            this.Controls.Add(this.wbMain);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FacebookLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Facebook Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wbMain;
+    }
+}
---
+++ FacebookLogin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ADown
+{
+    public partial class FacebookLogin : Form
+    {
+        public string AccessToken;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FacebookLogin"/> class.
+        /// Automatically navigates to the OAuth login page
+        /// </summary>
+        public FacebookLogin()
+        {
+            InitializeComponent();
+            AccessToken = "";
+            wbMain.Navigate("https://www.facebook.com/dialog/oauth/?client_id=122417977901113&response_type=token&redirect_uri=https://www.facebook.com/connect/login_success.html&display=popup&scope=user_photos,friends_photos");
+
+        }
+
+        /// <summary>
+        /// Handles the Navigated event of the wbMain control.
+        /// This will catch all post-navigation events and will check for the access token on each page URL
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="WebBrowserNavigatedEventArgs"/> instance containing the event data.</param>
+        private void wbMain_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            // Match the access_token variable in the URL
+            Match m = Regex.Match(e.Url.ToString(), "#access_token=(.+?)&");
+
+            // If access_token variable is found, assign it to AccessToken attribute
+            if (m.Success)
+            {
+                AccessToken = m.Groups[1].Value.ToString();
+                this.Close();
+            }
+        }
+
+
+    }
+}
